e3ed39923748f333983b068f7d50532366d92efc
Marcel Potinga <marcelpotinga@MacBook-Pro-di-Marcel.local>
2026-01-22 13:05:33 +0100

    Assesment of the models

--- Project.R
+++ Project.R
@@ -157,35 +157,185 @@ test_std_bc <- predict(train_only_std_bc, test)
 
 #Modelling on DT MS no Scaling
 
-metric <- "Spec"
-control <- trainControl(method="cv", number=10, summaryFunction = twoClassSummary, classProbs = TRUE,savePrediction = TRUE)
-random_forest <- train(is_fake~., data=train_dt, method="rf", metric=metric, ntree=250, trControl=control)
-
-decision_tree <- train(is_fake ~ ., data = train_dt, method = "rpart",metric=metric,
-      tuneLength = 10,
-      trControl = control)
-
-xgboost_model <- train(is_fake ~ ., data = train_dt, method = "xgbTree", trControl = control, metric = metric, use_rmm=TRUE, verbose=0)
-
-logistic_regression <- train(is_fake ~ ., data = train_dt, method = "glm",family = "binomial",trControl = control,metric=metric,preProcess = c("center", "scale")
-)
-
-library(nnet)
-ann_model <- train(is_fake ~ ., data = train_dt, method = "nnet",trControl = control,metric=metric,preProcess = c("center", "scale"), trace = FALSE, linout = FALSE, maxit = 200)
-save(random_forest,decision_tree,xgboost_model,logistic_regression,ann_model, file="models_DT_NoPreProc.Rdata")
-
+# metric <- "ROC"
+# control <- trainControl(method="cv", number=10, summaryFunction = twoClassSummary, classProbs = TRUE,savePrediction = TRUE)
+# random_forest <- train(is_fake~., data=train_dt, method="rf", metric=metric, ntree=250, trControl=control)
+# decision_tree <- train(is_fake ~ ., data = train_dt, method = "rpart",metric=metric,
+#        tuneLength = 10,
+#        trControl = control)
+# xgboost_model <- train(is_fake ~ ., data = train_dt, method = "xgbTree", trControl = control, metric = metric, use_rmm=TRUE, verbose=0)
+# logistic_regression <- train(is_fake ~ ., data = train_dt, method = "glm",family = "binomial",trControl = control,metric=metric,preProcess = c("center", "scale"))
+# library(nnet)
+# #ann_model <- train(is_fake ~ ., data = train_dt, method = "nnet",trControl = control,metric="ROC",preProcess = c("center", "scale"), trace = FALSE, linout = FALSE, maxit = 200)
+# save(random_forest,decision_tree,xgboost_model,logistic_regression,ann_model, file="models_DT_NoPreProc.Rdata")
+load("models_DT_NoPreProc.Rdata")
 #Modelling on Lasso MS no SCALING
-random_forest_lasso <- train(is_fake~., data=train_lasso, method="rf", metric=metric, ntree=250, trControl=control)
+# random_forest_lasso <- train(is_fake~., data=train_lasso, method="rf", metric=metric, ntree=250, trControl=control)
+# decision_tree_lasso <- train(is_fake ~ ., data = train_lasso, method = "rpart",metric=metric,
+#                         tuneLength = 10,
+#                        trControl = control)
+# xgboost_model_lasso <- train(is_fake ~ ., data = train_lasso, method = "xgbTree", trControl = control, metric = metric, use_rmm=TRUE, verbose=0)
+# logistic_regression_lasso <- train(is_fake ~ ., data = train_lasso, method = "glm",family = "binomial",trControl = control,metric=metric,preProcess = c("center", "scale"))
+# ann_model_lasso <- train(is_fake ~ ., data = train_lasso, method = "nnet",trControl = control,metric="ROC",preProcess = c("center", "scale"), trace = FALSE, linout = FALSE, maxit = 200)
+# save(random_forest_lasso,decision_tree_lasso,xgboost_model_lasso,logistic_regression_lasso,ann_model_lasso, file="models_Lasso_NoPreProc.Rdata")
+load("models_Lasso_NoPreProc.Rdata")
 
-decision_tree_lasso <- train(is_fake ~ ., data = train_lasso, method = "rpart",metric=metric,
-                       tuneLength = 10,
-                       trControl = control)
+#Modelling on DT + STD
+# random_forest_dt_std <- train(is_fake~., data=train_dt_std, method="rf", metric=metric, ntree=250, trControl=control)
+# decision_tree_dt_std <- train(is_fake ~ ., data = train_dt_std, method = "rpart",metric=metric,
+#                         tuneLength = 10,
+#                         trControl = control)
+# xgboost_model_dt_std <- train(is_fake ~ ., data = train_dt_std, method = "xgbTree", trControl = control, metric = metric, use_rmm=TRUE, verbose=0)
+# save(random_forest_dt_std,decision_tree_dt_std,xgboost_model_dt_std, file="models_DT_STD.Rdata")
+load("models_DT_STD.Rdata")
 
-xgboost_model_lasso <- train(is_fake ~ ., data = train_lasso, method = "xgbTree", trControl = control, metric = metric, use_rmm=TRUE, verbose=0)
+#Modelling on Lasso + STD
+# random_forest_lasso_std <- train(is_fake~., data=train_lasso_std, method="rf", metric=metric, ntree=250, trControl=control)
+# decision_tree_lasso_std <- train(is_fake ~ ., data = train_lasso_std, method = "rpart",metric=metric,
+#                         tuneLength = 10,
+#                         trControl = control)
+# xgboost_model_lasso_std <- train(is_fake ~ ., data = train_lasso_std, method = "xgbTree", trControl = control, metric = metric, use_rmm=TRUE, verbose=0)
+# save(random_forest_lasso_std,decision_tree_lasso_std,xgboost_model_lasso_std, file="models_Lasso_STD.Rdata")
+load("models_Lasso_STD.Rdata")
 
-logistic_regression_lasso <- train(is_fake ~ ., data = train_lasso, method = "glm",family = "binomial",trControl = control,metric=metric,preProcess = c("center", "scale")
+#Modelling on DT + STD + bc
+# random_forest_dt_std_bc <- train(is_fake~., data=train_dt_std_bc, method="rf", metric=metric, ntree=250, trControl=control)
+# decision_tree_dt_std_bc <- train(is_fake ~ ., data = train_dt_std_bc, method = "rpart",metric=metric,
+#                         tuneLength = 10,
+#                         trControl = control)
+# xgboost_model_dt_std_bc <- train(is_fake ~ ., data = train_dt_std_bc, method = "xgbTree", trControl = control, metric = metric, use_rmm=TRUE, verbose=0)
+# logistic_regression_dt_std_bc <- train(is_fake ~ ., data = train_dt_std_bc, method = "glm",family = "binomial",trControl = control,metric=metric)
+# ann_model_dt_std_bc <- train(is_fake ~ ., data = train_dt_std_bc, method = "nnet",trControl = control,metric="ROC", trace = FALSE, linout = FALSE, maxit = 200)
+# save(random_forest_dt_std_bc,decision_tree_dt_std_bc,xgboost_model_dt_std_bc, file="models_DT_STD_BC.Rdata")
+load("models_DT_STD_BC.Rdata")
+
+#Modelling on Lasso + STD + bc
+# random_forest_lasso_std_bc <- train(is_fake~., data=train_lasso_std_bc, method="rf", metric=metric, ntree=250, trControl=control)
+# decision_tree_lasso_std_bc <- train(is_fake ~ ., data = train_lasso_std_bc, method = "rpart",metric=metric,
+#                         tuneLength = 10,
+#                         trControl = control)
+# xgboost_model_lasso_std_bc <- train(is_fake ~ ., data = train_lasso_std_bc, method = "xgbTree", trControl = control, metric = metric, use_rmm=TRUE, verbose=0)
+# logistic_regression_lasso_std_bc <- train(is_fake ~ ., data = train_lasso_std_bc, method = "glm",family = "binomial",trControl = control,metric=metric)
+# ann_model_lasso_std_bc <- train(is_fake ~ ., data = train_lasso_std_bc, method = "nnet",trControl = control,metric="ROC", trace = FALSE, linout = FALSE, maxit = 400)
+# save(random_forest_lasso_std_bc,decision_tree_lasso_std_bc,xgboost_model_lasso_std_bc,logistic_regression_lasso_std_bc, ann_model_lasso_std_bc, file="models_Lasso_STD_BC.Rdata")
+load("models_Lasso_STD_BC.Rdata")
+
+#MODELLING only on raw train data
+# random_forest_raw <- train(is_fake~., data=train_std, method="rf", metric=metric, ntree=250, trControl=control)
+# decision_tree_raw <- train(is_fake ~ ., data = train_std, method = "rpart",metric=metric,
+#                         tuneLength = 10,
+#                         trControl = control)
+# xgboost_model_raw <- train(is_fake ~ ., data = train_std, method = "xgbTree", trControl = control, metric = metric, use_rmm=TRUE, verbose=0)
+# logistic_regression_raw <- train(is_fake ~ ., data = train_std, method = "glm",family = "binomial",trControl = control,metric=metric,preProcess = c("center", "scale"))
+# ann_model_raw <- train(is_fake ~ ., data = train_std, method = "nnet",trControl = control,metric="ROC",preProcess = c("center", "scale"), trace = FALSE, linout = FALSE, maxit = 200)
+# save(random_forest_raw,decision_tree_raw,xgboost_model_raw,logistic_regression_raw,ann_model_raw, file="models_Raw_NoPreProc.Rdata")
+load("models_Raw_NoPreProc.Rdata")
+
+#Model Assessment
+#We will create a list of all the models
+model_list <- list(
+  random_forest,
+  decision_tree,
+  xgboost_model,
+  logistic_regression,
+  ann_model,
+  
+  random_forest_lasso,
+  decision_tree_lasso,
+  xgboost_model_lasso,
+  logistic_regression_lasso,
+  ann_model_lasso,
+  
+  random_forest_dt_std,
+  decision_tree_dt_std,
+  xgboost_model_dt_std,
+  
+  random_forest_lasso_std,
+  decision_tree_lasso_std,
+  xgboost_model_lasso_std,
+  
+  random_forest_dt_std_bc,
+  decision_tree_dt_std_bc,
+  xgboost_model_dt_std_bc,
+  logistic_regression_lasso_std_bc,
+  ann_model_dt_std_bc,
+  
+  random_forest_lasso_std_bc,
+  decision_tree_lasso_std_bc,
+  xgboost_model_lasso_std_bc,
+  logistic_regression_lasso_std_bc,
+  ann_model_lasso_std_bc,
+  
+  random_forest_raw,
+  decision_tree_raw,
+  xgboost_model_raw,
+  logistic_regression_raw,
+  ann_model_raw
 )
 
-library(nnet)
-ann_model_lasso <- train(is_fake ~ ., data = train_lasso, method = "nnet",trControl = control,metric=metric,preProcess = c("center", "scale"), trace = FALSE, linout = FALSE, maxit = 200)
-save(random_forest_lasso,decision_tree_lasso,xgboost_model_lasso,logistic_regression_lasso,ann_model_lasso, file="models_Lasso_NoPreProc.Rdata")
+results <- resamples(model_list, modelNames=model_names)
+bwplot(results, 
+       horizontal = TRUE,           # Modelli verticali, metriche orizzontali
+       layout = c(3, 1),            # 3 grafici in riga (ROC | Sens | Spec)
+       scales = list(x = list(relation = "free")),  # Scale indipendenti
+       panel = function(...) {      # Custom panel per sorting
+         panel.bwplot(...)
+       })
+cat("Lasso solo:", max(xgboost_model_lasso$results$Sens), "\n")
+cat("XGB post-Lasso:", max(xgboost_model$results$Sens), "\n")
+
+
+preds_class_lasso <- predict(xgboost_model_lasso, test_lasso, type = "raw")  # Factor classi
+#Confusion matrix Lasso
+conf_mat <- confusionMatrix(preds_class, test_lasso$is_fake, 
+                            positive = "one")
+
+preds_class_dt <- predict(xgboost_model, test_dt, type = "raw")  # Factor classi
+#Confusion matrix XGB DT
+conf_mat <- confusionMatrix(preds_class, test_dt$is_fake, 
+                            positive = "one")  # "one"=fake
+print(conf_mat)
+
+#We will compare LIFT Charts
+library(funModeling)
+probs_xgb <- predict(xgboost_model, test_dt, type = "prob")[, "one"]
+test_dt$postXGB <- probs_xgb 
+xgb_lift <- gain_lift(data = test_dt, score = 'postXGB', target = 'is_fake')
+plot(xgb_lift)  # Lift curve
+print(xgb_lift)
+
+#Lasso Lift Chart
+probs_xgb_lasso <- predict(xgboost_model_lasso, test_lasso, type = "prob")[, "one"]
+test_lasso$postXGB <- probs_xgb_lasso
+xgb_lift_lasso <- gain_lift(data = test_lasso, score = 'postXGB', target = 'is_fake')
+plot(xgb_lift_lasso)  # Lift curve
+print(xgb_lift_lasso)
+
+#XGB lasso winning model NO scaling on variables.
+probs <- test_lasso$postXGB
+roc_obj <- roc(test_lasso$is_fake, probs, 
+               levels = c("zero", "one"), 
+               direction = "<", quiet = TRUE)  
+coords(roc_obj, "best", ret=c("threshold", "sensitivity", "specificity"))
+
+#PLOT ROC
+plot(roc_obj, 
+     main = "ROC Curve - XGB Lasso (4 feat)", 
+     print.auc = TRUE,           # AUC in legenda
+     print.thres = "best",       # Soglia Youden ottimale
+     col = "blue", lwd = 2)
+
+
+my_threshold <- 0.359  # O 0.8, 0.9, ecc.
+
+#Predictions
+preds_custom <- ifelse(probs > my_threshold, "one", "zero")
+conf_custom <- confusionMatrix(
+  factor(preds_custom, levels = c("zero", "one")), 
+  factor(test_lasso$is_fake, levels = c("zero", "one")), 
+  positive = "one"  # "one" = fake
+)
+
+# 5. Stampa risultati chiave
+print(conf_custom)
+
